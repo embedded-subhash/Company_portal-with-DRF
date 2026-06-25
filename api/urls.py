57a54a62c05ac0c43db2ad9184b4383d897b01d1
Subhash <malesubhash70@gmail.com>
2026-06-25 11:16:30 +0530

    api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
 
+    path(
+        'employees/',
+        views.EmployeeListCreateAPIView.as_view(),
+        name='employee_list_create'
+    ),
+
+    path(
+        'employees/<int:pk>/',
+        views.EmployeeDetailAPIView.as_view(),
+        name='employee_detail'
+    ),
 ]
